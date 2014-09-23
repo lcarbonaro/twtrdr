18a62e0f11da379eb8bf31080742a9a3239054a5
lcarbonaro <lcarbonaro@gmail.com>
2014-09-23 16:12:27 +0000

    latest edits

--- rss/twitter_auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-$twitter_auth = array(			// Copy and paste in your Twitter app's ...
-	'consumer_key'    => '******************************',	// consumer key
-	'consumer_secret' => '******************************',	// consumer secret
-	'user_token'      => '******************************',	// access token
-	'user_secret'     => '******************************',	// access token secret
-)
-?>
